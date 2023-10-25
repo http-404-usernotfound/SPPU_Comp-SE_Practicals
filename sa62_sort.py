431e22d2e545e4da4b293599415c37db842a3c37
http-404-usernotfound <107795857+http-404-usernotfound@users.noreply.github.com>
2023-10-25 16:50:05 +0530

    sa62_sort.py

--- sa62_sort.py
+++ sa62_sort.py
@@ -1,45 +1,63 @@
 def acceptData(arr):
-	size = int(input("Enter number of students: "))
-	print("Enter percentages of the students:")
-	for i in range(size):
-		arr.append(float(input("> ")))
-	return size
+    size = int(input("Enter number of students: "))
+    print("Enter percentages of the students:")
+    for i in range(size):
+        arr.append(float(input("> ")))
+    return size
 
 
 def bubbleSort(arr, size):
-	pass
+    for Pass in range(size):
+        for j in range(size - 1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
 
 
 def selectionSort(arr, size):
-	pass
-	
+    for i in range(size):
+        if arr[0] > arr[i]:
+            temp = arr[i]
+            arr[i] = arr[0]
+            arr[0] = temp
+    
+    i = 1
+    while i < size - 1:
+        for j in range(i+1, size):
+            if arr[i]>arr[j]:
+                 temp = arr[i]
+                 arr[i] = arr[j]
+                 arr[j] = temp
+        i+=1
 
 
 def main():
-	size = None
-	scores = []
-	while True:
-		choice = int(input('''
+    size = None
+    scores = []
+    while True:
+        choice = int(input('''
   (1) - Accept Score
   (2) - Bubble Sort
   (3) - Selection Sort
   (4) - Display array
   (5) - Exit
-	Enter yout choice: '''))
-	
-		if choice == 1:
-			size = acceptData(scores)
-		elif choice == 2:
-			bubbleSort(scores, size)
-		elif choice == 3:
-			selectionSort(scores, size)
-		elif choice == 4:
-			print("Array is:", scores)
-		elif choice == 5:
-			print("Exiting...")
-			break
-		else:
-			print("Enter a valid input!")
+    Enter yout choice: '''))
+    
+        if choice == 1:
+            scores = []
+            size = acceptData(scores)
+        elif choice == 2:
+            bubbleSort(scores, size)
+        elif choice == 3:
+            selectionSort(scores, size)
+        elif choice == 4:
+            print("Array is:", scores)
+        elif choice == 5:
+            print("Exiting...")
+            break
+        else:
+            print("Enter a valid input!")
 
 
 main()
