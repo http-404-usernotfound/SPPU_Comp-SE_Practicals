80a4320fc835ebf7815590008f9b5bc2e6515cf1
http-404-usernotfound <107795857+http-404-usernotfound@users.noreply.github.com>
2023-10-28 14:40:10 +0530

    sa62_File.cpp

--- sa62_File.cpp
+++ sa62_File.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : sa62_File.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include<fstream>
 
