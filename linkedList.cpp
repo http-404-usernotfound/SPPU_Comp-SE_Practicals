2b6e78baa40b6af044bff1c4ed35c8c5b64a486f
http-404-usernotfound <107795857+http-404-usernotfound@users.noreply.github.com>
2023-10-30 11:00:29 +0530

    linkedList.cpp

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : DSL-linkedList.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
@@ -21,49 +13,87 @@ public:
 		next = NULL;
 	}
 
+	~node(){
+		cout<<"\nMember deleted.\n";
+	}
+
 };
 
 class Club{
-	static int count;
+	int count;
 public:
 	node *head, *tail;
 
 	Club(){
 		head = tail = NULL;
+		count = 0;
 	}
 
 
-	void addNode(string prn, string name){
+	void addMem(string prn, string name){
 		node *newNode = new node(prn, name);
 		if(head == NULL){
-			head = newNode;
-			tail = newNode;
+			head = new node("-", "-");
+			head->next = newNode;
+			newNode->next = tail;
 		}
 		else{
-			tail->next = newNode;
-			tail = newNode;
+			node *curr = head;
+			while(curr->next != tail) curr = curr->next;
+
+			curr->next = newNode;
+			newNode->next = tail;
 		}
 		count++;
-		node *curr = head;
-		while(curr->next->next != tail) curr = curr->next;
+	}
 
-		tail->next = curr->next;
-		curr->next = tail;
-		tail->next->next = NULL;
-		tail = tail->next;
+	void remMem(string prn){
+		node *curr = head;
+		while(curr->next->prn != prn) curr = curr->next;
+
+		if (curr->next->prn == prn){
+			node *temp = curr->next;
+
+			curr->next = curr->next->next;
+
+			delete temp;
+
+		}
+		else{
+			cout<<"\nMember not found!\n";
+		}
+	}
+
+	void addPres(string prn, string name){
+		node *temp = head;
+		head = new node(prn, name);
+		head->next = temp->next;
+
 
 	}
+	void remPres(){head = NULL;}
+
+	void addSec(string prn, string name){
+		tail = new node(prn, name);
+	}
+	void remSec(){tail = NULL;}
 
 	void display(){
 		node *curr = head;
-		while(curr != NULL){
+		cout<<"PRN: "<<curr->prn;
+		cout<<"  Name: "<<curr->name<<endl<<endl;
+		do{
+			curr = curr->next;
+
+			if(curr == NULL)cout<<"NULL"<<endl;
+			else{
 			cout<<"PRN: "<<curr->prn;
 			cout<<"  Name: "<<curr->name<<endl<<endl;
-			curr = curr->next;
-		}
+			}
+		}while(curr != NULL);
 	}
 
-	static int getCount(){
+	int getCount(){
 		return count;
 	}
 
@@ -76,20 +106,81 @@ public:
 	}
 };
 
-int Club::count = 0;
-
 int main() {
 	string name, prn;
 	Club c;
+	int choice;
+	int i, j;
+	i = j = 1;
 
-	cout<<"Enter the details()\n(enter '-1' when done)\n\n";
-	while(name != "-1"){
-		cout<<"Enter name: "; cin>>name;
-		cout<<"Enter PRN: "; cin>>prn;
-		if (prn != "-1") c.addNode(prn, name);
+
+//	while(i){
+//		cout<<"(1) - "
+//	}
+
+
+	while(j){
+		cout<<"============MENU===============\n";
+		cout<<"(1) - Add Members\n";
+		cout<<"(2) - Remove Member\n";
+		cout<<"(3) - Add President\n";
+		cout<<"(4) - Remove President\n";
+		cout<<"(5) - Add Secretary\n";
+		cout<<"(6) - Remove Secretary\n";
+		cout<<"(7) - Display No. of Members\n";
+		cout<<"(8) - Display members.\n";
+		cout<<"(9) - Exit.\n";
+		cout<<"\nEnter your choice: "; cin>>choice;
+
+		switch(choice){
+		case 1:
+			int num;
+			cout<<"How many members to enter?: "; cin>>num;
+			for(int k = 0; k<num; k++){
+				cout<<"Enter details:\n\nName: "; cin>>name;
+				cout<<"PRN: "; cin>>prn;
+				c.addMem(prn, name);
+			}
+			break;
+
+		case 2:
+			cout<<"Enter prn: "; cin>> prn;
+			c.remMem(prn);
+			break;
+
+		case 3:
+			cout<<"Enter details of President\n\nName "; cin>>name;
+			cout<<"PRN: "; cin>>prn;
+			c.addPres(prn, name);
+			break;
+
+		case 4:
+			cout<<"\n Removing President...";
+			c.remPres();
+			break;
+
+		case 5:
+			cout<<"Enter details of Secretary\n\nName: "; cin>>name;
+			cout<<"PRN: "; cin>>prn;
+			c.addSec(prn, name);
+			break;
+
+		case 6:
+			cout<<"Removing Secretary...";
+			c.remSec();
+			break;
+
+		case 7:
+			cout<<"Total number of members: "<<c.getCount();
+			break;
+
+		case 8:
+			c.display();
+			break;
+
+		case 9:
+			cout<<"\nExiting...\n";
+			return 0;
+		}
 	}
-
-	c.display();
-	return 0;
 }
-
