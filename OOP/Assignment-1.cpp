e73ff3a237a81f59ddbee6fc3f33ee6361c1bfd0
http-404-usernotfound <107795857+http-404-usernotfound@users.noreply.github.com>
2024-01-11 20:17:25 +0530

    Update Assignment-1.cpp

--- OOP/Assignment-1.cpp
+++ OOP/Assignment-1.cpp
@@ -9,49 +9,55 @@ public:
 	Complex(int imag = 0, int real = 0){
 		this->imag = imag;
 		this->real = real;
-		cout<<"Default constructor: ";this->giveOutput();
 	}
-	void takeInput(){
-		cin>> real; cin>> imag;
-	}
-
-	void giveOutput(){
-		cout<<real<<"+"<<imag<<'i'<<endl;
-	}
+	
 	Complex operator +(Complex comp){
 		Complex sum = comp;
 		sum.real+=real;
 		sum.imag+=imag;
 		return sum;
 	}
+	
 	Complex operator *(Complex comp){
 		Complex product;
 		product.real = real*comp.real-imag*comp.imag;
 		product.imag = real*comp.imag+imag*comp.real;
 
 		return product;
-
 	}
+	
+	friend istream& operator>>(istream&, Complex&);
+	friend ostream& operator<<(ostream&, Complex&);
 };
 
-int main() {
-	Complex comp1, comp2, sum, product;
-	cout<<"Enter first complex number:"<<endl;
-	comp1.takeInput();
-	cout<<"First complex number is: "; comp1.giveOutput();
-	cout<<endl;
-
-	cout<<"Enter second complex number:"<<endl;
-	comp2.takeInput();
-	cout<<"Second complex number is: "; comp2.giveOutput();
-	cout<<endl;
-
-	sum = comp1 + comp2;
-	product = comp1 * comp2;
-
-	cout<<"Sum is: "; sum.giveOutput();
+istream& operator>>(istream& in, Complex& comp){
+	in>>comp.real>>comp.imag;
 	
-	cout<<"Product is: "; product.giveOutput();
+	return in;
+}
+
+ostream& operator<<(ostream& out, Complex& comp){
+	out<<comp.real<<(comp.imag>=0?" +":"")<<comp.imag<<'i';
+		
+	return out;
+}
+
+int main() {
+	Complex comp1, comp2;
+	cout<<"Enter first complex number:"; cin>>comp1;
+	
+	cout<<"First complex number is: "<<comp1<<endl;
+
+	cout<<"Enter second complex number:"; cin>>comp2;
+	
+	cout<<"Second complex number is: "<<comp2<<endl;
+
+	Complex sum = comp1 + comp2;
+	Complex product = comp1 * comp2;
+
+	cout<<"Sum is: "<<sum<<endl;
+	
+	cout<<"Product is: "<<product<<endl;
 
 	return 0;
 }
